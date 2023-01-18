ce296b47b23203a41ab1738c17a5730ae8661213
Mit <53411468+mitkotak@users.noreply.github.com>
2023-01-18 14:50:27 -0600

    fixed typo

--- lookaround.sh
+++ lookaround.sh
@@ -2,10 +2,10 @@
 # Looks around the container
 mybreak="\n\n---------------Break-------------------\n\n"
 printf "Checking /compute-shared\n"
-ls /compute-shared
+ls /compute_shared
 
-printf "${mybreak}Writing to /compute-shared/hello.txt\n"
-echo "echo hello world" > /compute-shared/hello.txt
+printf "${mybreak}Writing to /compute_shared/hello.txt\n"
+echo "echo hello world" > /compute_shared/hello.txt
 
-printf "${mybreak}Reading from /compute-shared/hello.txt\n"
-cat /compute-shared/hello.txt
+printf "${mybreak}Reading from /compute_shared/hello.txt\n"
+cat /compute_shared/hello.txt
